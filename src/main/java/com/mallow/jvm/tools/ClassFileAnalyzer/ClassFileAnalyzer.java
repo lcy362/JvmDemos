3f0f623f5f4a763c6ea57647dddf1f5976d1cdaa
lcy362 <lcy362@163.com>
2017-03-09 19:15:31 +0800

    读二进制文件；byte[], 16进制转换

---
+++ src/main/java/com/mallow/jvm/tools/ClassFileAnalyzer/ClassFileAnalyzer.java
@@ -0,0 +1,23 @@
+package com.mallow.jvm.tools.ClassFileAnalyzer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+
+/**
+ * Created by lcy on 2017/3/9.
+ */
+public class ClassFileAnalyzer {
+    public static void main(String args[]) throws IOException {
+        File file = new File("TestClass.class");
+        FileInputStream inputStream = new FileInputStream(file);
+        byte[] bytes = new byte[2000];
+        inputStream.read(bytes);
+        System.out.println(binary(bytes, 16));
+    }
+
+    public static String binary(byte[] bytes, int radix){
+        return new BigInteger(1, bytes).toString(radix);// 这里的1代表正数
+    }
+}
