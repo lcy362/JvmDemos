e9a445ec8161018fb58e0b89368c28befcb74632
lcy362 <lcy362@163.com>
2017-03-13 20:03:07 +0800

    fieldref, classinfo

--- src/main/java/com/mallow/jvm/tools/ClassFileAnalyzer/ClassFileAnalyzer.java
+++ src/main/java/com/mallow/jvm/tools/ClassFileAnalyzer/ClassFileAnalyzer.java
@@ -32,12 +32,17 @@ public class ClassFileAnalyzer {
             ConstantPoolInfo poolInfo = ConstantPoolInfo.fromTag(tag);
             System.out.println(poolInfo);
             switch (poolInfo) {
-                case CONSTANT_METHODREF_INFO:
+                case CONSTANT_METHODREF_INFO : case CONSTANT_FIELDREF_INFO:
                     byte[] classIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
                     byte[] nameAndTypeIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
                     System.out.println("class index: " + binary(classIndex, 10));
                     System.out.println("name and type index: " + binary(nameAndTypeIndex, 10));
                     break;
+                case CONSTANT_CLASS_INFO:
+                    byte[] index = Arrays.copyOfRange(bytes, curse, curse+=2);
+                    System.out.println("index: " + binary(index, 10));
+                    break;
+
             }
         }
     }
