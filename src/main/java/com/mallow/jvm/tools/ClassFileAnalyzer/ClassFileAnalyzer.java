280d928165d8ec88828005bb23e0b7e58ec12414
joeng <lcy362@163.com>
2017-04-04 22:05:28 +0800

    save constant pool in a list

--- src/main/java/com/mallow/jvm/tools/ClassFileAnalyzer/ClassFileAnalyzer.java
+++ src/main/java/com/mallow/jvm/tools/ClassFileAnalyzer/ClassFileAnalyzer.java
@@ -33,7 +33,6 @@ public class ClassFileAnalyzer {
             byte[] constanTag = Arrays.copyOfRange(bytes, curse, curse+=1);
             int tag = Integer.parseInt(binary(constanTag, 10));
             ConstantPoolInfo poolInfo = ConstantPoolInfo.fromTag(tag);
-            System.out.println(i + "th: " + poolInfo);
             constant.setType(poolInfo);
             Map<String, Integer> refs = new HashMap<>();
             constant.setRefs(refs);
@@ -41,60 +40,64 @@ public class ClassFileAnalyzer {
                 case CONSTANT_METHODREF_INFO: case CONSTANT_FIELDREF_INFO:
                     byte[] classIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
                     byte[] nameAndTypeIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("class index: " + binary(classIndex, 10));
-                    System.out.println(binaryToDecimal(classIndex));
-                    System.out.println("name and type index: " + binary(nameAndTypeIndex, 10));
-                    System.out.println(binaryToDecimal(nameAndTypeIndex));
+                    refs.put("class index", binaryToDecimal(classIndex));
+                    refs.put("name and type index", binaryToDecimal(nameAndTypeIndex));
                     break;
                 case CONSTANT_CLASS_INFO: case CONSTANT_STRING_INFO:
                     byte[] index = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("index: " + binary(index, 10));
+                    refs.put("index", binaryToDecimal(index));
                     break;
                 case CONSTANT_INTEGER_INFO: case CONSTANT_LONG_INFO: case CONSTANT_DOUBLE_INFO:
                     byte[] value4 = Arrays.copyOfRange(bytes, curse, curse+=4);
-                    System.out.println("value: " + binary(value4, 10));
+                    constant.setInfo(binary(value4, 10));
+//                    System.out.println("value: " + binary(value4, 10));
                     break;
                 case CONSTANT_FLOAT_INFO:
                     byte[] value2 = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("value: " + binary(value2, 10));
+                    constant.setInfo(binary(value2, 10));
+//                    System.out.println("value: " + binary(value2, 10));
                     break;
                 case CONSTANT_UTF8_INFO:
                     byte[] length = Arrays.copyOfRange(bytes, curse, curse+=2);
                     int intLength = Integer.parseInt(binary(length, 10));
-                    System.out.println("utf8 length: " + binary(length, 10));
+//                    System.out.println("utf8 length: " + binary(length, 10));
                     byte[] utf8Value = Arrays.copyOfRange(bytes, curse, curse+=intLength);
-                    System.out.println("utf8 value: " + new String(utf8Value, "UTF-8"));
+//                    System.out.println("utf8 value: " + new String(utf8Value, "UTF-8"));
+                    constant.setInfo(new String(utf8Value, "UTF-8"));
                     break;
                 case CONSTANT_NAMEANDTYPE_INFO:
                     byte[] nameIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
                     byte[] descriptorIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("nameIndex: " + binary(nameIndex, 10));
-                    System.out.println("descriptorIndex: " + binary(descriptorIndex, 10));
+                    refs.put("nameIndex", binaryToDecimal(nameIndex));
+                    refs.put("descriptorIndex", binaryToDecimal(descriptorIndex));
                     break;
                 case CONSTANT_METHODHANDLE_INFO:
                     byte[] referenceIndex1 = Arrays.copyOfRange(bytes, curse, curse+=1);
                     byte[] referenceIndex2 = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("referenceIndex1: " + binary(referenceIndex1, 10));
-                    System.out.println("referenceIndex2: " + binary(referenceIndex2, 10));
+                    refs.put("referenceIndex1", binaryToDecimal(referenceIndex1));
+                    refs.put("referenceIndex2", binaryToDecimal(referenceIndex2));
                     break;
                 case CONSTANT_METHODTYPE_INFO:
                     byte[] descriptorIndex2 = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("descriptorIndex: " + binary(descriptorIndex2, 10));
+                    refs.put("descriptorIndex", binaryToDecimal(descriptorIndex2));
                     break;
                 case CONSTANT_INVOKEDYNAMIC_INFO:
                     byte[] bootstrapMethodAttrIndex = Arrays.copyOfRange(bytes, curse, curse+=2);
                     byte[] nameAndTypeIndex2 = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("bootstrapMethodAttrIndex: " + binary(bootstrapMethodAttrIndex, 10));
-                    System.out.println("nameAndTypeIndex: " + binary(nameAndTypeIndex2, 10));
+                    refs.put("bootstrapMethodAttrIndex", binaryToDecimal(bootstrapMethodAttrIndex));
+                    refs.put("nameAndTypeIndex", binaryToDecimal(nameAndTypeIndex2));
                     break;
                 case CONSTANT_INTERFACEMETHODREF_INFO:
                     byte[] classIndex2 = Arrays.copyOfRange(bytes, curse, curse+=2);
                     byte[] nameAndTypeIndexU2 = Arrays.copyOfRange(bytes, curse, curse+=2);
-                    System.out.println("nameIndex: " + binary(classIndex2, 10));
-                    System.out.println("descriptorIndex: " + binary(nameAndTypeIndexU2, 10));
+                    refs.put("nameIndex", binaryToDecimal(classIndex2));
+                    refs.put("descriptorIndex", binaryToDecimal(nameAndTypeIndexU2));
                     break;
             }
             pool.add(constant);
+        }
+        for (int i = 1; i < constantSize; i++) {
+            System.out.println(i + "th constant: " + pool.get(i));
         }
     }
 
