f9ef397a84f6b0b2ff6fe7477871b31c7c0a38cc
lcy362 <lcy362@163.com>
2017-04-05 20:16:13 +0800

    accsessflag info

---
+++ src/main/java/com/mallow/jvm/tools/ClassFileAnalyzer/AccessFlags.java
@@ -0,0 +1,22 @@
+package com.mallow.jvm.tools.ClassFileAnalyzer;
+
+/**
+ * Created by lcy on 2017/4/5.
+ */
+public class AccessFlags {
+    public static final short ACC_PUBLIC = 0x0001;
+    public static final short ACC_PRIVATE = 0x0002;
+    public static final short ACC_PROTECTED = 0x0004;
+    public static final short ACC_STATIC = 0x0008;
+    public static final short ACC_FINAL = 0x0010;
+    public static final short ACC_SUPER = 0x0020;
+    public static final short ACC_VOLATILE = 0x0040;
+    public static final short ACC_TRANSIENT = 0x0080;
+    public static final short ACC_NATIVE = 0x0100;
+    public static final short ACC_INTERFACE = 0x0200;
+    public static final short ABSTRACT = 0x0400;
+    public static final short ACC_STRICTFP = 0x0800;
+    public static final short ACC_SYNTHETIC = 0x1000;
+    public static final short ACC_ANNOTATION = 0x2000;
+    public static final short ACC_ENUM = 0x4000;
+}
